577ac3bb64b4f05f957afc00cc37197be40211ad
Mateusz_Maj <mateusz.maj.krakow@gmail.com>
2024-04-22 20:24:18 +0200

    add 2nd test case

--- test/numbers-validator/isNumberEven.spec.js
+++ test/numbers-validator/isNumberEven.spec.js
@@ -10,27 +10,27 @@ import NumbersValidator from '../../app/numbers-validator.js';
 
 // 'describe' is used to group related tests together into a test suite.
 // Here, it describes a suite of tests for the 'isNumberEven' method of NumbersValidator class.
-describe('isNumberEven', function () {
+describe('isNumberEven', () => {
   // Declaring a variable 'validator' outside of the 'beforeEach' and 'it' blocks
   // to make it accessible throughout this describe block.
   let validator;
 
   // 'beforeEach' is a hook that runs before each test ('it' block) within this 'describe' block.
   // It's usually used for setting up the test environment.
-  beforeEach(function () {
+  beforeEach(() => {
     // Instantiates a new NumbersValidator object before each test and assigns it to 'validator'
     validator = new NumbersValidator();
   });
 
   // 'afterEach' is a hook that runs after each test. It is often used for cleanup activities.
-  afterEach(function () {
+  afterEach(() => {
     // Sets the validator variable to null to clean up memory after each test
     validator = null;
   });
 
   // 'it' is used for individual test cases - it includes the actual test.
   // The string argument describes what the test should do.
-  it('should return true if number is even', function () {
+  it('should return true if number is even', () => {
     // Using 'expect' to assert that the 'isNumberEven' method returns true when
     // passed the even number 4. The '.to.be.equal(true)' is the actual assertion check.
     expect(validator.isNumberEven(4)).to.be.equal(true);
@@ -38,4 +38,7 @@ describe('isNumberEven', function () {
 
   // Additional tests would follow for different test cases, such as testing if an odd number
   // returns false or if passing a non-number throws an error.
+  it('should return false if number is odd', () => {
+    expect(validator.isNumberEven(5)).to.be.equal(false);
+  })
 });
